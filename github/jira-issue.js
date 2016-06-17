0586d996f1498f23c2cfa4a37cda85e826a49178
Massimiliano Cannarozzo <maxcanna@gmail.com>
2016-06-17 10:52:55 +0200

    Use waitForKeyElements
    GitHub does not reload the page moving beetween tabs so DOM doesn't get updated if we run the script only on page load.
    This way we wait for item visibility and we inject our element

--- github/jira-issue.js
+++ github/jira-issue.js
@@ -5,16 +5,18 @@
 // @description  Linkifies branch name
 // @author       Massimiliano Cannarozzo
 // @include      /^https://github.com/adespresso/adespresso/pull/\d+/
-// @grant        none
-// @require http://code.jquery.com/jquery-latest.js
+// @require      http://code.jquery.com/jquery-latest.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
-(function() {
-    'use strict';
+function linkifyBranchName() {
     var elem = $(".current-branch:contains('AAPP')").first();
     var text = elem.text();
     var issue = /^.+(AAPP-\d+)$/.exec(text)[1];
     var url = 'https://adespresso.atlassian.net/browse/'+issue;
     elem.attr('title', null);
     elem.html('<a href="'+url+'" target="_blank" title="See issue on Jira">'+elem.text()+'</a>');
+}
+(function() {
+    waitForKeyElements(".current-branch:contains('AAPP')", linkifyBranchName);
 })();
 
